977073befd8e3884a7113fa74cdee38f79f013d3
Rajshrishrimalshah <raj.shrishrimalshah@successive.tech>
2019-04-26 10:22:06 +0530

    feat:program complete(39520)

---
+++ extraTs/utils/helpers.ts
@@ -0,0 +1,19 @@
+// export const validateEmail = (users) => {
+//   let myEmail = /@successive.tech\s*$/;
+
+//   return users.match(myEmail);
+
+
+//   };
+export function validateEmail(users){
+  console.log(users)
+  let myEmail = /@successive.tech\s*$/;
+
+  return users.match(myEmail);
+
+
+  };
+
+
+
+
