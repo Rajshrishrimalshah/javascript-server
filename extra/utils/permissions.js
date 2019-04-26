963641adec4733f4758d9103736efbe8d8a848c5
Rajshrishrimalshah <raj.shrishrimalshah@successive.tech>
2019-04-26 16:05:25 +0530

    fix:changes

--- extra/utils/permissions.js
+++ extra/utils/permissions.js
@@ -1,27 +1,38 @@
-import {PERMISSION_ALL,PERMISSION_READ,PERMISSION_WRITE,PERMISSION_DELETE,
-  MODULE_USER,ROLE_OF_TRAINER,ROLE_OF_TRAINEE,ROLL_OF_HEAD_TRAINER} from "./../constant"
+import {
+  PERMISSION_ALL,
+  PERMISSION_READ,
+  PERMISSION_WRITE,
+  PERMISSION_DELETE,
+  MODULE_USER,
+  ROLE_OF_TRAINER,
+  ROLE_OF_TRAINEE,
+  ROLL_OF_HEAD_TRAINER
+} from "./../constant";
 
 const PERMISSION = {
-  [MODULE_USER] : {
-		[PERMISSION_ALL]: [ROLL_OF_HEAD_TRAINER],
-		[PERMISSION_READ]: [ROLE_OF_TRAINEE, ROLE_OF_TRAINER],
-		[PERMISSION_WRITE]: [ROLE_OF_TRAINER],
-		[PERMISSION_DELETE]: [],
-	},
+  [MODULE_USER]: {
+    [PERMISSION_ALL]: [ROLL_OF_HEAD_TRAINER],
+    [PERMISSION_READ]: [ROLE_OF_TRAINEE, ROLE_OF_TRAINER],
+    [PERMISSION_WRITE]: [ROLE_OF_TRAINER],
+    [PERMISSION_DELETE]: []
+  }
 };
 
-export const  hasPermission = (module,role,permissionType) => {
-  if(!PERMISSION[module]) { return false;
+export const hasPermission = (module, role, permissionType) => {
+  if (!PERMISSION[module]) {
+    return false;
   }
 
-  if(!PERMISSION[module][permissionType]) {  return false;
-}
-
-  if(PERMISSION[module][PERMISSION_ALL] && PERMISSION[module][PERMISSION_ALL] == -1) {
+  if (!PERMISSION[module][permissionType]) {
     return false;
-}
+  }
+
+  if (
+    PERMISSION[module][PERMISSION_ALL] &&
+    PERMISSION[module][PERMISSION_ALL] == -1
+  ) {
+    return false;
+  }
 
   return true;
 };
-
-
