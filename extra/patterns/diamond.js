d0d764813ddad0f885dd15c4b8df860526b9343b
Rajshrishrimalshah <raj.shrishrimalshah@successive.tech>
2019-04-26 16:35:13 +0530

    fix:changes

--- extra/patterns/diamond.js
+++ extra/patterns/diamond.js
@@ -1,4 +1,4 @@
-export const diamond = (max) => {
+export const diamond = max => {
   let row, column;
   if (max < 2 || max > 10) {
     console.log(" Please enter the number between 2 to 10 only !");
--- extra/utils/helpers.js
+++ extra/utils/helpers.js
@@ -1,19 +1,6 @@
-// export const validateEmail = (users) => {
-//   let myEmail = /@successive.tech\s*$/;
-
-//   return users.match(myEmail);
-
-
-//   };
-export function validateEmail(users){
-  console.log(users)
+export function validateEmail(users) {
+  console.log(users);
   let myEmail = /@successive.tech\s*$/;
 
   return users.match(myEmail);
-
-
-  };
-
-
-
-
+}
