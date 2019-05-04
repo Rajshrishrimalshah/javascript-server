2ee788116f51faed8f582c5a722d7af961a326e7
Rajshrishrimalshah <raj.shrishrimalshah@successive.tech>
2019-05-04 09:56:19 +0530

    style: Indentation

--- src/libs/routes/authMoiddleWare.ts
+++ src/libs/routes/authMoiddleWare.ts
@@ -1,7 +1,6 @@
 import * as jwt from "jsonwebtoken";
-import  { default as hasPermission } from "../../../extraTs/utils/permissions";
+import { default as hasPermission } from "../../../extraTs/utils/permissions";
 import { configuration } from "../../config";
-
 
 export const authMiddleWare = (module, permissionType) => (req, res, next) => {
   const bearerHeader = req.headers["authorization"];
@@ -12,18 +11,25 @@ export const authMiddleWare = (module, permissionType) => (req, res, next) => {
 
     jwt.verify(token, configuration.secret, function(err, decode) {
       if (err) {
-        next({error: 'Unauthorized', status: 403, message: 'Your are unauthorized'});
-      }else {
-          if(hasPermission(module, decode.role, permissionType)){
-              next();
-          }else{
-            next({error: 'Unauthorized', status: 403, message: 'Your are unauthorized'});
-          }
-
-            next();
+        next({
+          error: "Unauthorized",
+          status: 403,
+          message: "Your are unauthorized"
+        });
+      } else {
+        if (hasPermission(module, decode.role, permissionType)) {
+          next();
+        } else {
+          next({
+            error: "Unauthorized",
+            status: 403,
+            message: "Your are unauthorized"
+          });
+        }
+        next();
       }
     });
-  }else{
+  } else {
     res.sendStatus(403);
   }
-  }
+};
